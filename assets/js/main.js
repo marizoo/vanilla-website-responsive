c088949b17ac2a56672477e5255da18a6f288b1f
marizoo <marizoo@meow.woof>
2022-03-21 17:23:22 +0700

    section: Header: 100%

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,50 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
+// ----------- Menu Show ----------
+// validate if constant exists
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
+}
+// ----------- Menu Hidden ----------
+// validate if constant exists & close menu when close icon is clicked
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
+function linkAction() {
+    const navMenu = document.getElementById("nav-menu");
+    // when we click on each "nav__link", we remove the "show-menu" class
+    navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader() {
+    const header = document.getElementById("header");
+    // when the scroll is greater than 50vh, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== POPULAR SWIPER ===============*/
 
-
 /*=============== MIXITUP FILTER FEATURED ===============*/
 
+/* Link active featured */
 
-/* Link active featured */ 
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
